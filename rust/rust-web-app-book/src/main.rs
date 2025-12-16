9b9e8d387f879fa5c2b5ad7aab80172456cff747
kyu08 <49891479+kyu08@users.noreply.github.com>
2025-12-16 20:36:49 +0900

    [chapter3] add: /hello

--- rust/rust-web-app-book/src/main.rs
+++ rust/rust-web-app-book/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{Ipv4Addr, SocketAddr};
+
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/hello", get(hello_world));
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("Listening on {}", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
