4df714fa8f9fd13f7441fc0c3a9f1880b4087a51
kyu08 <49891479+kyu08@users.noreply.github.com>
2025-12-17 00:00:46 +0900

    setup each layers

---
+++ rust/rust-web-app-book/shared/src/config.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+
+pub struct AppConfig {
+    pub database: DatabaseConfig,
+}
+
+impl AppConfig {
+    pub fn new() -> Result<Self> {
+        let database = DatabaseConfig {
+            host: "localhost".into(),
+            port: 5432,
+            username: "app".into(),
+            password: "passwd".into(),
+            database: "app".into(),
+        };
+        Ok(Self { database })
+    }
+}
+
+pub struct DatabaseConfig {
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub database: String,
+}
