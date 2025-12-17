735473521dcbccbb801fbb3d2a80c0c544a72c00
kyu08 <49891479+kyu08@users.noreply.github.com>
2025-12-17 18:47:13 +0900

    router, handler

--- rust/rust-web-app-book/src/bin/app.rs
+++ rust/rust-web-app-book/src/bin/app.rs
@@ -27,18 +27,6 @@ async fn main() -> Result<()> {
     Ok(axum::serve(listener, app).await?)
 }
 
-async fn health_check() -> StatusCode {
-    StatusCode::OK
-}
-
-async fn health_check_db(State(db): State<PgPool>) -> StatusCode {
-    let connection_result = sqlx::query("SELECT 1").fetch_one(&db).await;
-    match connection_result {
-        Ok(_) => StatusCode::OK,
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
-    }
-}
-
 struct DatabaseConfig {
     pub host: String,
     pub port: u16,
