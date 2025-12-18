144aceae864907da6f42076b546ca7cbbd2baa74
kyu08 <49891479+kyu08@users.noreply.github.com>
2025-12-18 23:38:53 +0900

    env, log

--- rust/rust-web-app-book/src/bin/app.rs
+++ rust/rust-web-app-book/src/bin/app.rs
@@ -1,16 +1,45 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
 use adapter::database::connect_database_with;
-use anyhow::Result;
+use anyhow::{Context, Result};
 use api::route::{book::build_book_router, health::build_health_check_routers};
 use axum::Router;
 use registry::AppRegistry;
-use shared::config::AppConfig;
+use shared::{
+    config::AppConfig,
+    env::{Environment, which},
+};
 use tokio::net::TcpListener;
+use tower_http::{
+    LatencyUnit, cors,
+    trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
+};
+use tracing::Level;
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    init_logger()?;
     bootstrap().await
+}
+
+fn init_logger() -> Result<()> {
+    let log_level = match which() {
+        Environment::Development => "debug",
+        Environment::Production => "info",
+    };
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
+
+    let subscriber = tracing_subscriber::fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false);
+    tracing_subscriber::registry()
+        .with(subscriber)
+        .with(env_filter)
+        .try_init()?;
+
+    Ok(())
 }
 
 async fn bootstrap() -> Result<()> {
@@ -20,11 +49,31 @@ async fn bootstrap() -> Result<()> {
     let app = Router::new()
         .merge(build_health_check_routers())
         .merge(build_book_router())
+        .layer(cors())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(LatencyUnit::Millis),
+                ),
+        )
         .with_state(registry);
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(addr).await?;
-    println!("Listening on {}", addr);
+    tracing::info!("Listening on {}", addr);
 
-    Ok(axum::serve(listener, app).await?)
+    axum::serve(listener, app)
+        .await
+        .context("Unexpected error happened in server")
+        .inspect_err(|e| {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "Unexpected error"
+            )
+        })
 }
