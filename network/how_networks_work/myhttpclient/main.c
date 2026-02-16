898cc1aa1273e0d7e98c7fcaf144528200a75a59
kyu08 <49891479+kyu08@users.noreply.github.com>
2026-02-16 20:37:52 +0900

    add comment

--- network/how_networks_work/myhttpclient/main.c
+++ network/how_networks_work/myhttpclient/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
+  // ドメイン名からIPアドレスを取得する
+  // ソケットを作成し、接続先を登録する
+  // ソケットに接続する
+  // http requestを実行する
+  // responseを読み込む
+  // closeする
   int number = 1;
   printf("hi %d", number);
   return 0;
